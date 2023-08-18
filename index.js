49b83f56d82057a3adfb988af60780a4e4c0570a
babakdevgeek <babak.dev.geek@gmail.com>
2023-08-18 16:00:13 +0330

    icon became button

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function triger() {
   document.getElementById("navigation").classList.toggle("responsive");
 }
+const nav = document.getElementById("navigation");
+const links = nav.getElementsByTagName("a");
+const current = nav.getElementsByClassName("active");
+console.log(links);
+for (let i = 0; i > links.length; i++) {
+  links[i].addEventListener("click", () => {
+    if (current.length > 0) {
+      current[0].className = current[0].classList.remove("active");
+    }
+    console.log(this);
+    this.className += " active";
+  });
+}
